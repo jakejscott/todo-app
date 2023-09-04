800796162c4e046dfc95f9bffc4283af16d56300
Jake Scott <jake.net@gmail.com>
2023-09-04 13:34:43 +1200

    tweak

--- apps/backend/TodoList.Api.UnitTests/TestApplication.cs
+++ apps/backend/TodoList.Api.UnitTests/TestApplication.cs
@@ -7,7 +7,10 @@ public class TestApplication : WebApplicationFactory<Program>
     protected override IHost CreateHost(IHostBuilder builder)
     {
         // NOTE: Load environment variables from .env file
-        DotEnv.Fluent().WithTrimValues().WithOverwriteExistingVars().WithProbeForEnv(8).Load();
+        DotEnv.Fluent()
+            .WithTrimValues()
+            .WithProbeForEnv(8)
+            .Load();
         
         var connectionString = Env.GetString("PLANET_SCALE_CONNECTION_STRING");
         var version = new MySqlServerVersion(ServerVersion.AutoDetect(connectionString));
